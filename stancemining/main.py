Fixing transcription func, scaling clustering, and others

stancemining/main.py
@@ -57,7 +57,8 @@ class StanceMining:
             embedding_model_inference='vllm',
             topic_model='toponymy',
             cosine_similarity_threshold=0.8,
-            verbose=False
+            verbose=False,
+            use_embedding_cache=True,
         ):
         """Initialize the StanceMining class.
         """
@@ -128,12 +129,21 @@ class StanceMining:
         assert topic_model in ['bertopic', 'toponymy'], f"Topic model must be either 'bertopic' or 'toponymy', not '{topic_model}'"
         self.topic_model = topic_model
 
+        self.use_embedding_cache = use_embedding_cache
+
     def _generate_higher_level_targets(self, document_df: pl.DataFrame, embed_model, topic_model_kwargs, max_layers):
         logger.info("Fitting topic model")
-        base_target_df = document_df.select(['Targets']).explode('Targets').rename({'Targets': 'Target'}).unique('Target').drop_nulls('Target')
+        # get unique targets where most common targets are first
+        base_target_df = document_df.select(['Targets'])\
+            .explode('Targets')\
+            .rename({'Targets': 'Target'})\
+            .drop_nulls('Target')\
+            .group_by('Target')\
+            .len()\
+            .sort('len', descending=True)\
+            .drop('len')
         doc_targets = base_target_df['Target'].to_list()
-        embeddings = self._get_embeddings(doc_targets, model=embed_model)
-        cluster_layers, cluster_df = self._topic_model(doc_targets, embeddings, embed_model, topic_model_kwargs, max_layers)
+        cluster_layers, cluster_df = self._topic_model(doc_targets, embed_model, topic_model_kwargs, max_layers)
         if len(cluster_df) > 0:
             base_target_cluster_df = base_target_df.with_columns(
                 pl.Series(name='Clusters', values=np.stack(cluster_layers, axis=-1), dtype=pl.Array(pl.Int64, len(cluster_layers)))\
@@ -218,8 +228,8 @@ class StanceMining:
         if not generate_targets:
             assert len(targets) > 0, "If not generating targets, you must provide a list of targets."
 
-        if generate_targets:
-            assert len(targets) == 0, "If generating targets, the targets argument must be an empty list."
+        if targets and not generate_targets:
+            logger.info("Using provided targets")
 
         if embedding_cache is not None:
             assert isinstance(embedding_cache, pl.DataFrame), "embedding_cache must be a polars DataFrame"
@@ -298,27 +308,29 @@ class StanceMining:
     def _get_embeddings(self, docs: Union[List[str], pl.Series], model=None) -> np.ndarray:
         if model is None:
             model = self._get_embedding_model()
-        # check for cached embeddings
-        if isinstance(docs, pl.Series):
-            document_df = docs.rename('text').to_frame()
+        if self.use_embedding_cache:
+            # check for cached embeddings
+            if isinstance(docs, pl.Series):
+                document_df = docs.rename('text').to_frame()
+            else:
+                document_df = pl.DataFrame({'text': docs})
+            document_df = document_df.join(self.embedding_cache_df, on='text', how='left', maintain_order='left')
+            missing_docs = document_df.unique('text').filter(pl.col('embedding').is_null())
+            if len(missing_docs) > 0:
+                logger.debug(f"Computing embeddings for {len(missing_docs)} missing documents")
+                new_embeddings = model.encode(missing_docs['text'].to_list(), show_progress_bar=self.verbose).astype(np.float32)
+                
+                missing_docs = missing_docs.with_columns(pl.Series(name='embedding', values=new_embeddings))
+                # cache embeddings
+                logger.debug("Updating embedding cache")
+                self.embedding_cache_df = pl.concat([self.embedding_cache_df, missing_docs], how='diagonal_relaxed')
+                # add new embeddings to document_df
+                document_df = document_df.join(missing_docs, on='text', how='left', maintain_order='left')\
+                    .with_columns(pl.coalesce(['embedding', 'embedding_right']))\
+                    .drop('embedding_right')
+            embeddings = document_df['embedding'].to_numpy()
         else:
-            document_df = pl.DataFrame({'text': docs})
-        document_df = document_df.join(self.embedding_cache_df, on='text', how='left', maintain_order='left')
-        missing_docs = document_df.unique('text').filter(pl.col('embedding').is_null())
-        if len(missing_docs) > 0:
-            logger.debug(f"Computing embeddings for {len(missing_docs)} missing documents")
-            # batch process missing docs to try to avoid OOM
-            new_embeddings = model.encode(missing_docs['text'].to_list(), show_progress_bar=self.verbose).astype(np.float32)
-            
-            missing_docs = missing_docs.with_columns(pl.Series(name='embedding', values=new_embeddings))
-            # cache embeddings
-            logger.debug("Updating embedding cache")
-            self.embedding_cache_df = pl.concat([self.embedding_cache_df, missing_docs], how='diagonal_relaxed')
-            # add new embeddings to document_df
-            document_df = document_df.join(missing_docs, on='text', how='left', maintain_order='left')\
-                .with_columns(pl.coalesce(['embedding', 'embedding_right']))\
-                .drop('embedding_right')
-        embeddings = document_df['embedding'].to_numpy()
+            embeddings = model.encode(docs, show_progress_bar=self.verbose).astype(np.float32)
 
         return embeddings
 
@@ -458,11 +470,11 @@ class StanceMining:
         )
         return documents_df
 
-    def _topic_model(self, targets, embeddings, embedding_model, kwargs, max_layers):
+    def _topic_model(self, targets, embedding_model, kwargs, max_layers):
         if self.topic_model == 'toponymy':
-            results = self._toponymy_topic_model(targets, embeddings, embedding_model, kwargs, max_layers)
+            results = self._toponymy_topic_model(targets, embedding_model, kwargs, max_layers)
         elif self.topic_model == 'bertopic':
-            results = self._bertopic_topic_model(targets, embeddings, embedding_model, kwargs)
+            results = self._bertopic_topic_model(targets, embedding_model, kwargs)
         else:
             raise ValueError(f"Unknown topic model: {self.topic_model}. Supported models: 'toponymy', 'bertopic'.")
 
@@ -471,9 +483,10 @@ class StanceMining:
 
         return results
         
-    def _bertopic_topic_model(self, targets, embeddings, embedding_model, kwargs):
+    def _bertopic_topic_model(self, targets, embedding_model, kwargs):
         try:
             from bertopic import BERTopic
+            from bertopic.backend._utils import select_backend
         except ImportError:
             raise ImportError("BERTopic package is not installed. Please install it with 'pip install bertopic'.")
 
@@ -502,9 +515,55 @@ class StanceMining:
                 from cuml.cluster import HDBSCAN
                 kwargs['hdbscan_model'] = HDBSCAN(min_samples=10, gen_min_span_tree=True, prediction_data=True, verbose=self.verbose, random_state=42)
         topic_model = BERTopic(**kwargs)
-        topics, probs = topic_model.fit_transform(targets, embeddings=embeddings)
+        doc_ids = range(len(targets))
 
-        base_target_df = pl.DataFrame({'Topic': topics, 'Document': targets}).with_row_index(name='ID')
+        import pandas as pd
+        document_df = pd.DataFrame({"Document": targets, "ID": doc_ids, "Topic": None})
+
+        self.embedding_model = select_backend(
+            self.embedding_model, language=topic_model.language, verbose=self.verbose
+        )
+
+        batch_size = 2500000
+        if len(targets) <= batch_size:
+            embeddings = self._get_embeddings(targets, model=embed_model)
+
+            # Reduce dimensionality and fit UMAP model
+            umap_embeddings = topic_model._reduce_dimensionality(embeddings)
+        else:
+            # batch process to try to avoid OOM
+            embeddings = None
+            umap_embeddings = None
+            for i in tqdm(range(0, len(targets), batch_size), desc="Computing embeddings for targets in batches"):
+                batch_texts = targets[i:i+batch_size]
+                batch_embeddings = self._get_embeddings(batch_texts, model=embedding_model)
+
+                if i == 0:
+                    batch_umap_embeddings = umap_model.fit_transform(batch_embeddings)
+                else:
+                    batch_umap_embeddings = umap_model.transform(batch_embeddings)
+                if umap_embeddings is None:
+                    umap_embeddings = batch_umap_embeddings
+                else:
+                    umap_embeddings = np.vstack([umap_embeddings, batch_umap_embeddings])
+
+        document_df, _ = topic_model._cluster_embeddings(umap_embeddings, document_df)
+
+        # Sort and Map Topic IDs by their frequency
+        if not topic_model.nr_topics:
+            document_df = topic_model._sort_mappings_by_frequency(document_df)
+
+        # Extract topics by calculating c-TF-IDF, reduce topics if needed, and get representations.
+        topic_model._extract_topics(
+            document_df, embeddings=None, verbose=topic_model.verbose, fine_tune_representation=not topic_model.nr_topics
+        )
+        if topic_model.nr_topics:
+            document_df = topic_model._reduce_topics(document_df)
+
+        # Save the top 3 most representative documents per topic
+        topic_model._save_representative_docs(document_df)
+
+        base_target_df = pl.from_pandas(document_df)
         num_representative_docs = 5
         repr_docs, _, _, _ = topic_model._extract_representative_docs(
             topic_model.c_tf_idf_,
@@ -519,9 +578,9 @@ class StanceMining:
             .rename({'Representation': 'Keyphrases', 'Representative_Docs': 'Exemplars', 'Topic': 'Cluster'})\
             .filter(pl.col('Cluster') != -1)
 
-        return [np.array(topics)], cluster_df
+        return [base_target_df['Topic'].to_numpy()], cluster_df
 
-    def _toponymy_topic_model(self, targets, embeddings, embedding_model, kwargs, max_layers):
+    def _toponymy_topic_model(self, targets, embedding_model, kwargs, max_layers):
         try:
             import toponymy
         except ImportError:
@@ -556,6 +615,8 @@ class StanceMining:
         else:
             import pacmap
             umap_model = pacmap.PaCMAP(**umap_kwargs)
+
+        embeddings = self._get_embeddings(doc_targets, model=embed_model)
         clusterable_vectors = umap_model.fit_transform(embeddings)
 
         # running toponymy fit method except topic naming step which we do ourselves

stancemining/utils.py
@@ -1,6 +1,6 @@
 import gc
 import logging
-import tempfile
+import subprocess
 from typing import List
 
 from nltk.corpus import stopwords
@@ -447,7 +447,8 @@ def get_transcripts_from_video_files(
         whisper_model: str = "large-v2", 
         batch_size: int = 16, 
         save_speaker_embeddings: bool = False, 
-        verbose: bool = True
+        verbose: bool = True,
+        skip_errors: bool = False
     ) -> pl.DataFrame:
     """Get transcripts from a list of video file paths using whisperx.
 
@@ -466,6 +467,7 @@ def get_transcripts_from_video_files(
     """
     try:
         import whisperx
+        from whisperx.audio import SAMPLE_RATE
     except ImportError:
         raise ImportError("whisperx is not installed. Please install it with `pip install whisperx`.")
 
@@ -478,15 +480,27 @@ def get_transcripts_from_video_files(
             "-f", "s16le",
             "-ac", "1",
             "-acodec", "pcm_s16le", 
-            "-ar", str(sr),
+            "-ar", str(SAMPLE_RATE),
             "-"
         ]
         
-        result = subprocess.run(cmd, capture_output=True, check=True)
+        try:
+            result = subprocess.run(cmd, capture_output=True, check=True)
+        except subprocess.CalledProcessError as e:
+            raise RuntimeError(f"ffmpeg command failed with error: {e.stderr.decode()}") from e
         audio = np.frombuffer(result.stdout, np.int16).flatten().astype(np.float32) / 32768.0
         return audio
 
-    return _get_transcripts_from_audio(video_paths, load_audio_from_video_file, hf_token=hf_token, whisper_model=whisper_model, batch_size=batch_size, verbose=verbose, save_speaker_embeddings=save_speaker_embeddings)
+    return _get_transcripts_from_audio(
+        video_paths, 
+        load_audio_from_video_file, 
+        hf_token=hf_token, 
+        whisper_model=whisper_model, 
+        batch_size=batch_size, 
+        verbose=verbose, 
+        save_speaker_embeddings=save_speaker_embeddings,
+        skip_errors=skip_errors
+    )
 
 def get_transcripts_from_audio_files(
         audio_paths: List[str], 
@@ -494,7 +508,8 @@ def get_transcripts_from_audio_files(
         whisper_model: str = "large-v2", 
         batch_size: int = 16, 
         save_speaker_embeddings: bool = False, 
-        verbose: bool = True
+        verbose: bool = True,
+        skip_errors: bool = False
     ) -> pl.DataFrame:
     """Get transcripts from a list of audio file paths using whisperx.
 
@@ -520,7 +535,16 @@ def get_transcripts_from_audio_files(
         audio = whisperx.load_audio(audio_file)
         return audio
 
-    return _get_transcripts_from_audio(audio_paths, load_audio_from_file, hf_token=hf_token, whisper_model=whisper_model, batch_size=batch_size, verbose=verbose, save_speaker_embeddings=save_speaker_embeddings)
+    return _get_transcripts_from_audio(
+        audio_paths, 
+        load_audio_from_file, 
+        hf_token=hf_token, 
+        whisper_model=whisper_model, 
+        batch_size=batch_size, 
+        verbose=verbose, 
+        save_speaker_embeddings=save_speaker_embeddings,
+        skip_errors=skip_errors
+    )
 
 def _get_transcripts_from_audio(
         items,
@@ -529,7 +553,8 @@ def _get_transcripts_from_audio(
         whisper_model="large-v2", 
         batch_size=16, 
         save_speaker_embeddings=False,
-        verbose=True
+        verbose=True,
+        skip_errors=False
     ) -> pl.DataFrame:
     import torch
 
@@ -540,53 +565,70 @@ def _get_transcripts_from_audio(
     except ImportError:
         raise ImportError("whisperx and/or pyannote is not installed. Please install them with `pip install whisperx pyannote.audio`.")
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu") 
+    device_name = "cuda" if torch.cuda.is_available() else "cpu"
+    device = torch.device(device_name)
     compute_type = "float16" # change to "int8" if low on GPU mem (may reduce accuracy)
-    model = whisperx.load_model(whisper_model, device, compute_type=compute_type)
+    model = whisperx.load_model(whisper_model, device_name, compute_type=compute_type)
     diarize_model = Pipeline.from_pretrained("pyannote/speaker-diarization-3.1", use_auth_token=hf_token).to(device)
 
     results = []
     for item in tqdm(items, disable=not verbose, desc="Transcribing"):
-        audio = audio_loader(item)
-        result = model.transcribe(audio, batch_size=batch_size)
+        try:
+            audio = audio_loader(item)
+            result = model.transcribe(audio, batch_size=batch_size)
 
-        # delete model if low on GPU resources
-        # import gc; gc.collect(); torch.cuda.empty_cache(); del model
+            # delete model if low on GPU resources
+            # import gc; gc.collect(); torch.cuda.empty_cache(); del model
 
-        # 2. Align whisper output
-        language = result['language']
-        model_a, metadata = whisperx.load_align_model(language_code=language, device=device)
-        result = whisperx.align(result["segments"], model_a, metadata, audio, device, return_char_alignments=False)
+            # 2. Align whisper output
+            language = result['language']
+            try:
+                model_a, metadata = whisperx.load_align_model(language_code=language, device=device)
+            except ValueError:
+                d = {
+                    'path': item,
+                    'result': result,
+                }
+                results.append(d)
+                continue
 
-        # delete model if low on GPU resources
-        # import gc; gc.collect(); torch.cuda.empty_cache(); del model_a
+            result = whisperx.align(result["segments"], model_a, metadata, audio, device, return_char_alignments=False)
 
-        # 3. Assign speaker labels
-        # add min/max number of speakers if known
-        audio_data = {
-            'waveform': torch.from_numpy(audio[None, :]),
-            'sample_rate': SAMPLE_RATE
-        }
-        segments, embeddings = diarize_model(audio_data, return_embeddings=True)
-        diarize_segments = pl.DataFrame(segments.itertracks(yield_label=True), columns=['segment', 'label', 'speaker'])
-        diarize_segments = diarize_segments.with_columns([
-            pl.col('segment').struct.field('start').alias('start'),
-            pl.col('segment').struct.field('end').alias('end')
-        ])
-        
-        result = whisperx.assign_word_speakers(diarize_segments, result)
-        result['language'] = language
+            # delete model if low on GPU resources
+            # import gc; gc.collect(); torch.cuda.empty_cache(); del model_a
 
-        d = {
-            'path': item,
-            'result': result,
-            'diarize_segments': diarize_segments,
-        }
+            # 3. Assign speaker labels
+            # add min/max number of speakers if known
+            audio_data = {
+                'waveform': torch.from_numpy(audio[None, :]),
+                'sample_rate': SAMPLE_RATE
+            }
+            segments, embeddings = diarize_model(audio_data, return_embeddings=True)
+            diarize_segments = pl.DataFrame(segments.itertracks(yield_label=True), schema=['segment', 'label', 'speaker'])
+            diarize_segments = diarize_segments.with_columns([
+                pl.col('segment').map_elements(lambda s: s.start, pl.Float64).alias('start'),
+                pl.col('segment').map_elements(lambda s: s.end, pl.Float64).alias('end')
+            ]).to_pandas()
+            
+            result = whisperx.assign_word_speakers(diarize_segments, result)
+            result['language'] = language
 
-        if save_speaker_embeddings:
-            d['embeddings'] = embeddings
+            d = {
+                'path': item,
+                'result': result,
+                'diarize_segments': diarize_segments[['label', 'start', 'end', 'speaker']].to_dict(orient='records'),
+            }
 
-        results.append(d)
+            if save_speaker_embeddings:
+                d['embeddings'] = embeddings
+
+            results.append(d)
+        except Exception as e:
+            if skip_errors:
+                logger.error(f"Error processing {item}: {e}")
+                continue
+            else:
+                raise
 
     return pl.DataFrame(results)
 